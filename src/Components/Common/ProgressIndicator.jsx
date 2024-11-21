a3345af071b0d4249e09071c8deab85daca967dd
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2024-11-21 17:41:18 +0100

    Genre section created, needs some clean up

---
+++ src/Components/Common/ProgressIndicator.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import "./styles.progressIndicator.sass";
+
+export const ProgressIndicator = ({ index }) => {
+	const [percentage, setPercentage] = useState(0);
+
+	useEffect(() => {
+		// Convert index (0-10) to percentage (0-100)
+		const percentage = (index / 10) * 100;
+		setPercentage(percentage);
+	}, [index]);
+
+	const getIndicatorStyle = () => {
+		return {
+			background: `conic-gradient(
+          red 0%,
+          yellow ${percentage / 2}%,
+          green ${percentage}%,
+          #EAEAEA ${percentage}%
+        ) content-box`,
+		};
+	};
+
+	return (
+		<div className="index-container">
+			<div className="indicator" style={getIndicatorStyle()}></div>
+			<p className="index-text">{percentage.toFixed(0)}%</p>
+		</div>
+	);
+};

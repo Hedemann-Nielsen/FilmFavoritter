32da6af00f09eb760db1d467f039560e1ae6b793
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2024-11-22 12:01:41 +0100

    #3 Key Features  section created

--- src/Components/Customers/Home/Home.jsx
+++ src/Components/Customers/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { useGenreList } from "../../Hooks/GenreMovieList";
 
 import { GenreSection } from "./GenreSection";
+import { KeyFeatures } from "./KeyFeatures";
 import { News } from "./News";
 
 export const Home = () => {
@@ -23,6 +24,15 @@ export const Home = () => {
 	const genreThrillerId = genreThriller?.id;
 	const genreThrillerName = genreThriller?.name;
 
+	// Find War-genre
+	const genreWar = genreList?.find((genre) => genre.name === "War");
+	const genreWarId = genreWar?.id;
+	const genreWarName = genreWar?.name;
+
+	// Find Romance-genre
+	const genreRomance = genreList?.find((genre) => genre.name === "Romance");
+	const genreRomanceId = genreRomance?.id;
+	const genreRomanceName = genreRomance?.name;
 	// Loading and error states
 	if (loadingGenre) return <p>Loading genres...</p>;
 	if (errorGengre) return <p>Error loading genres: {errorGengre}</p>;
@@ -33,6 +43,11 @@ export const Home = () => {
 			<GenreSection gereId={genreActionId} title={genreActionName} />
 			<GenreSection gereId={genreComedyId} title={genreComedyName} />
 			<GenreSection gereId={genreThrillerId} title={genreThrillerName} />
+			<div className="flex justify-center">
+				<KeyFeatures />
+			</div>
+			<GenreSection gereId={genreWarId} title={genreWarName} />
+			<GenreSection gereId={genreRomanceId} title={genreRomanceName} />
 		</>
 	);
 };
